fdb04fef9e39287120c9c884f4f63e17c35ac822
Eszter <ekov@protonmail.ch>
2017-05-13 21:17:42 +0200

    improve messages page

--- static/helpers.js
+++ static/helpers.js
@@ -1,4 +1,14 @@
 window.onload = () => {
   const inputField = document.querySelector('input');
   inputField && inputField.focus();
+
+  const messageInput = document.querySelector('textarea');
+  messageInput.onkeypress = (key) => {
+    if (!key.shiftKey && key.which == 13) {
+      send_message.submit();
+    }
+  }
+
+  const messageList = document.querySelector('.messages');
+  messageList.scrollTop = messageList.scrollHeight;
 }
