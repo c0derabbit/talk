d805c4bd0a58397f906d85ee070cf6d05e61a886
Eszter <ekov@protonmail.ch>
2017-05-31 08:53:18 +0200

    get db credentials from envvars

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 prod_env = dict(
-	DATABASE_URL='postgres://vvyvnusnxnfagn:f7093437d20db7220f34a36937223cef47cb313a25d3815a6ba544193bbd21f9@ec2-54-75-231-195.eu-west-1.compute.amazonaws.com:5432/dd6ub0ojujlq6q',
-	SECRET_KEY='blahwillsetthislater'
+	DATABASE_URL=os.environ.get('DATABASE_URL'),
+	SECRET_KEY=os.environ.get('SECRET_KEY')
 )
